4a47d997d46bfef7533e5a9e8b86f919504628cd
Oswaldo-Hqz <oehero@hotmail.com>
2022-06-05 23:56:36 -0600

    validation in user registration endpoints

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,14 @@ import { Router } from "express";
 const router = Router();
 
 import * as authCtrl from '../controllers/auth.controller'
+import {validationSignup} from '../middlewares'
 
-router.post('/register', authCtrl.register)
+router.post(
+  "/register",
+  [validationSignup.checkDuplicateEmail, validationSignup.checkRolesExisted],
+  authCtrl.register
+);
+
 router.post('/login', authCtrl.logIn)
 
 export default router;
