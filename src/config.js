78374d5710364559aa8833220254e974b8767a30
Oswaldo-Hqz <oehero@hotmail.com>
2022-06-05 22:00:13 -0600

    database connection setup

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
+import { config } from "dotenv";
+config();
+
 export default {
-    SECRET: 'HITEN-MITSURUGI'
+    SECRET: 'HITEN-MITSURUGI',
+    MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost/mylibrary",
 }
